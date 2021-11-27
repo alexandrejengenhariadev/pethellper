184cd9d11ca2e26cb998252f2a02f0b93a8b52e6
Alexandre Jesus <2008139@aluno.univesp.br>
2021-11-27 09:49:49 -0300

    Feito a corrção do codigo baixa.php

--- php_action/baixa.php
+++ php_action/baixa.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once 'db_connect.php';
-
 $baixa=null;
 if (isset($_POST['ckDoacao']))
     $baixa = $_POST['ckDoacao'];
@@ -9,28 +8,20 @@ if (isset($_POST['ckDoacao']))
 if ($baixa != null){
     for ($i=0; $i < count($baixa); $i++)
     {
-       
-        $baixa[$i] = $baixa[$i];
-        //echo $baixa[$i]."<br>";
+        //echo "<p>{$frutas[$i]}</p>";
+        $cad=$baixa[$i];
+        //echo "<p>{$cad}</p>";
+        $sql = "DELETE FROM doacao WHERE codigodoacao ='$cad'";
+        if(mysqli_query($connect, $sql)):
+            $_SESSION['mensagem'] = 'Baixa dada com sucesso!';
+            header('Location: ../ongAdm.php');
+        else:
+            $_SESSION['mensagem'] = 'Erro ao Dar baixa!';
+            header('Location: ../ongAdm.php');
+        endif;
+        
+
     }
 }
 
 
-
-if (isset($_POST['btnSubmit'])):
-    
-    $id = mysqli_escape_string($connect,$_POST['id']);
-
-    $sql = "DELETE FROM doacao WHERE codigodoacao ='$baixa[$i];'";
-
-    if(mysqli_query($connect, $sql)):
-        $_SESSION['mensagem'] = 'Baixa dada com sucesso!';
-        header('Location: ../ongAdm.php');
-    else:
-        $_SESSION['mensagem'] = 'Erro ao Dar baixa!';
-        header('Location: ../listaDoacao.php');
-    endif;
-   
-  
-   
-endif;
--- php_action/save.php
+++ php_action/save.php
@@ -6,9 +6,9 @@ if (isset($_POST['ckDoacao']))
 if ($baixa != null){
     for ($i=0; $i < count($baixa); $i++)
     {
-       
-        $baixa[$i] = $baixa[$i];
-        echo $baixa[$i]."<br>";
+        //echo "<p>{$frutas[$i]}</p>";
+        $cad=$baixa[$i];
+        echo "<p>{$cad}</p>";
     }
 }
 
